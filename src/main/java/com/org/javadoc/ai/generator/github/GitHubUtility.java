fa4ac412099661374d0d5b8925ed6c917cd6719d
shasanka2710 <41183085+shasanka2710@users.noreply.github.com>
2025-01-29 20:07:35 +0530

    Apply fix: Automated fixing issues

--- src/main/java/com/org/javadoc/ai/generator/github/GitHubUtility.java
+++ src/main/java/com/org/javadoc/ai/generator/github/GitHubUtility.java
@@ -3,7 +3,6 @@ package com.org.javadoc.ai.generator.github;
 import org.kohsuke.github.GHPullRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -11,10 +10,14 @@ import java.util.List;
 
 @Component
 public class GitHubUtility {
+
     private static final Logger logger = LoggerFactory.getLogger(GitHubUtility.class);
 
-    @Autowired
-    private PullRequestHandler pullRequestHandler;
+    private final PullRequestHandler pullRequestHandler;
+
+    public GitHubUtility(PullRequestHandler pullRequestHandler) {
+        this.pullRequestHandler = pullRequestHandler;
+    }
 
     public String createPullRequest(List<String> classNames, String description) throws IOException {
         try {
@@ -23,9 +26,9 @@ public class GitHubUtility {
                 throw new IOException("Error creating pull request");
             }
             logger.info("Pull Request Created:");
-            logger.info("Title: " + pullRequest.getTitle());
-            logger.info("Number: " + pullRequest.getNumber());
-            logger.info("URL: " + pullRequest.getHtmlUrl());
+            logger.info("Title: {}", pullRequest.getTitle()); // Format specifiers used here
+            logger.info("Number: {}", pullRequest.getNumber()); // Format specifiers used here
+            logger.info("URL: {}", pullRequest.getHtmlUrl()); // Format specifiers used here
             return pullRequest.getHtmlUrl().toString();
         } catch (IOException e) {
             throw new IOException("Error creating pull request", e);
