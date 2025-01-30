8c30730750eb988f24dd3acd3abefdc48cbe01ce
shasanka2710 <41183085+shasanka2710@users.noreply.github.com>
2025-01-30 17:16:08 +0530

    Apply fix: Automated fixing issues

--- src/main/java/com/org/javadoc/ai/generator/util/StringUtil.java
+++ src/main/java/com/org/javadoc/ai/generator/util/StringUtil.java
@@ -2,8 +2,12 @@ package com.org.javadoc.ai.generator.util;
 
 public class StringUtil {
 
+    private StringUtil() {
+        // Private constructor to hide the implicit public one
+    }
+
     public static String getclassDisplayName(String fullPackageName) {
-        if (fullPackageName!= null) {
+        if (fullPackageName != null) {
             // Find the last dot separator for the extension
             int lastDotIndex = fullPackageName.lastIndexOf(".");
             // Find the second last dot to locate the start of the file name
