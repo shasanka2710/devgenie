c4e23c6ebc893bac0ad0765e5c92ae447c3d1fab
shasanka2710 <shasanka.learning@gmail.com>
2025-02-03 02:00:31 +0530

    Modal Window beautification -- check point

--- src/main/java/com/org/javadoc/ai/generator/controller/IssueFixController.java
+++ src/main/java/com/org/javadoc/ai/generator/controller/IssueFixController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,8 +17,9 @@ import java.util.UUID;
 @RequestMapping("/sonar/issue")
 public class IssueFixController {
 
+    private static final Logger logger = LoggerFactory.getLogger(IssueFixController.class); // Reordered modifier
+
     private final IssueFixService fixService;
-    private final static Logger logger = LoggerFactory.getLogger(IssueFixController.class);
 
     public IssueFixController(IssueFixService fixService) {
         this.fixService = fixService;
